2a040b3c20a4c122b5fb95c2dac47125611965a9
baraaabuzeid <baraa@aleph-labs.com>
2022-02-11 17:04:49 +0200

    Init commit

---
+++ src/test/java/com/baraabytes/restaurantsApp/api/RestaurantsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baraabytes.restaurantsApp.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantsAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
