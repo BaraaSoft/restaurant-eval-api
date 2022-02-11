0a65300a4dd35ef560bda33e6d7f11f0b07e57cf
baraaabuzeid <baraa@aleph-labs.com>
2022-02-11 20:44:18 +0200

    Add restaurant service interfaces.

---
+++ src/main/java/com/baraabytes/restaurantsApp/api/services/RestaurantService.java
@@ -0,0 +1,22 @@
+package com.baraabytes.restaurantsApp.api.services;
+
+import com.baraabytes.restaurantsApp.api.entities.Restaurant;
+import com.baraabytes.restaurantsApp.api.repositories.RestaurantRepository;
+import com.baraabytes.restaurantsApp.api.types.WeekDayType;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public interface RestaurantService<R,E> {
+    R addRestaurant(R restaurant);
+    R findRestaurant(Long id);
+    List<R> findRestaurants(String name);
+    List<R> findRestaurants(WeekDayType weekDay);
+    List<R> findRestaurants(LocalDateTime openTime,LocalDateTime closeTime);
+    void removeRestaurant(Long id);
+
+
+    E addTimeEntry(E Schedule);
+    void removeTimeEntry(Long id);
+
+}
