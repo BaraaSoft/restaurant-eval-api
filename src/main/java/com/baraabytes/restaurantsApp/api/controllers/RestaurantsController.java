d06b95e4d8b7d1cfbe16d73a9f505351cc0143d8
baraaabuzeid <baraa@aleph-labs.com>
2022-02-11 18:39:35 +0200

    Add restaurants controller.

---
+++ src/main/java/com/baraabytes/restaurantsApp/api/controllers/RestaurantsController.java
@@ -0,0 +1,11 @@
+package com.baraabytes.restaurantsApp.api.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/restaurants")
+public class RestaurantsController {
+
+
+}
