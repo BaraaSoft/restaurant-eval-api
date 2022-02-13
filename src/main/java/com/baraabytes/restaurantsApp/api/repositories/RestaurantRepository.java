d1730a561603d136c197d75dc3f5fe8e116cbada
baraaabuzeid <baraa@aleph-labs.com>
2022-02-13 23:32:19 +0200

    restaurants endpoint to return pagination info.

--- src/main/java/com/baraabytes/restaurantsApp/api/repositories/RestaurantRepository.java
+++ src/main/java/com/baraabytes/restaurantsApp/api/repositories/RestaurantRepository.java
@@ -2,6 +2,7 @@ package com.baraabytes.restaurantsApp.api.repositories;
 
 
 import com.baraabytes.restaurantsApp.api.entities.Restaurant;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
@@ -10,5 +11,5 @@ import org.springframework.data.repository.PagingAndSortingRepository;
 import java.util.List;
 
 public interface RestaurantRepository extends PagingAndSortingRepository<Restaurant,Long> {
-    List<Restaurant> findAllByNameStartsWith(String name, Pageable pageable);
+    Page<Restaurant> findAllByNameStartsWith(String name, Pageable pageable);
 }
