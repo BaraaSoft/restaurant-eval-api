212aa3b0e3c14098101cc74e6abd13c04b8185d5
baraaabuzeid <baraa@aleph-labs.com>
2022-02-15 13:57:02 +0200

    init favourites RestController

---
+++ src/main/java/com/baraabytes/restaurantsApp/api/controllers/FavouriteController.java
@@ -0,0 +1,10 @@
+package com.baraabytes.restaurantsApp.api.controllers;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/user/{id}/favourites")
+public class FavouriteController {
+}
